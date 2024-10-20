4f01fabd6081d471b34d20f77ef297d5edfd2921
Ean Smith <eansmith@comcast.net>
2024-10-19 21:05:41 -0400

    projector prefab

--- Assets/Scripts/Screen.cs
+++ Assets/Scripts/Screen.cs
@@ -46,7 +46,7 @@ public class Screen : MonoBehaviour
             
             if(remainingTime >= 0)
             {
-                countdownText.text = $"Can proceed in\n{remainingTime:D} seconds";
+                countdownText.text = $"Can proceed in {remainingTime:D} seconds";
                 countdownText.gameObject.SetActive(true); // Show timer while waiting
             }
             if (remainingTime <= 0)
